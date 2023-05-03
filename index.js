ef9fa9fd71995dfc533279003cb56acb07f890d1
Sain Shabh <126852062+sainshabh900@users.noreply.github.com>
2023-05-03 14:19:58 +0530

    uploading files

---
+++ index.js
@@ -0,0 +1,51 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonClicked=this.innerHTML;
+        keypressed(buttonClicked);
+        keyAnimation(buttonClicked);
+    });
+
+}
+    addEventListener("keypress",function(event){
+        keypressed(event.key);
+        keyAnimation(event.key);
+    })
+    function keypressed(key){
+         switch(key){
+        case"w": var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+        case"a": var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+        case"s": var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+        case"d": var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+        case"j": var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+        case"k": var kickBass = new Audio('sounds/kick-Bass.mp3');
+        kickBass.play();
+        break;
+        case"l": var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+        default:
+        alert("wrong button clicked");
+
+    }
+
+    }
+
+function keyAnimation(btnkey){
+    var activeButton=document.querySelector("."+btnkey)
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    },100);
+
+}
